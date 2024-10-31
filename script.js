3adcb003678dd8152f7d3faea566d8bb41ec90e5
rishabhy043 <rishabh.knipss043@gmail.com>
2024-10-31 22:50:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var main =document.querySelector('.main');
+var crsr = document.querySelector('.cursor')
+
+main.addEventListener("mousemove" , function(e){
+    console.log(e)
+         crsr.style.left = e.x+"px";
+         crsr.style.top = e.y+"px"
+})
